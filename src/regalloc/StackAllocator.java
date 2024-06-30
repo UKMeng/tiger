d5638aa60cda5b9d8dc1d1873286193e55bd6b5f
UKMeng <ukmengse@gmail.com>
2024-06-30 17:08:07 +0800

    Lab5 PartA Exercise 4.

--- src/regalloc/StackAllocator.java
+++ src/regalloc/StackAllocator.java
@@ -16,10 +16,47 @@ import java.util.function.BiFunction;
 // using a stack-based allocation approach.
 public class StackAllocator {
 
-    private X64.Block.T allocBlock(X64.Block.T block, TempMap tempMap) {
+    private X64.Block.T allocBlock(X64.Block.T block, TempMap tempMap, boolean firstBlock, Frame frame) {
+        Id rbpReg = X64.Register.allRegs.get(6);
+        Id rspReg = X64.Register.allRegs.get(7);
         switch (block) {
             case X64.Block.Singleton(Label label, List<X64.Instr.T> instrs, List<X64.Transfer.T> transfer) -> {
                 List<X64.Instr.T> newInstrs = new ArrayList<>();
+
+                if (firstBlock) {
+                    // Prologue
+                    // Save base pointer register
+                    newInstrs.add(new X64.Instr.Singleton(X64.Instr.Kind.Store,
+                            (uarg, darg) -> STR."pushq\t%rbp",
+                            List.of(new X64.VirtualReg.Reg(rbpReg, new X64.Type.Int())),
+                            List.of()));
+
+
+                    // Save all Callee-Saved Registers
+                    for (Id reg : X64.Register.calleeSavedRegs) {
+                        List<X64.VirtualReg.T> uses = List.of(new X64.VirtualReg.Reg(reg, new X64.Type.Int()));
+                        List<X64.VirtualReg.T> defs = List.of();
+                        newInstrs.add(new X64.Instr.Singleton(X64.Instr.Kind.Store,
+                                (uarg, darg) -> STR."pushq\t\{uarg.getFirst()}",
+                                uses,
+                                defs));
+                    }
+
+                    // Move stack pointer register to base pointer register
+                    newInstrs.add(new X64.Instr.Singleton(X64.Instr.Kind.Move,
+                            (uarg, darg) -> STR."movq\t%rsp, %rbp",
+                            List.of(new X64.VirtualReg.Reg(rspReg, new X64.Type.Int())),
+                            List.of(new X64.VirtualReg.Reg(rbpReg, new X64.Type.Int()))));
+
+                    // Allocate space for local variables
+                    // Suppose `localsSize` is the total size of local variables
+                    newInstrs.add(new X64.Instr.Singleton(X64.Instr.Kind.Bop,
+                            (uarg, darg) -> STR."subq\t$" + frame.size() + ", %rsp",
+                            List.of(new X64.VirtualReg.Reg(rspReg, new X64.Type.Int())),
+                            List.of(new X64.VirtualReg.Reg(rspReg, new X64.Type.Int()))));
+                }
+
+
                 for (X64.Instr.T instr : instrs) {
                     switch (instr) {
                         case X64.Instr.Singleton(X64.Instr.Kind kind,
@@ -38,13 +75,11 @@ public class StackAllocator {
                                         regIndex++;
                                         regMap.put(id, reg);
 
-                                        Id rbpReg = X64.Register.allRegs.get(6);
-
                                         List<X64.VirtualReg.T> uses_ = List.of(new X64.VirtualReg.Reg(rbpReg, ty));
                                         List<X64.VirtualReg.T> defs_ = List.of(new X64.VirtualReg.Reg(reg, ty));
                                         newInstrs.add(new X64.Instr.Singleton(X64.Instr.Kind.Load,
                                                 (uarg, darg) ->
-                                                        STR."movq\t\{offset}(\{uarg.getFirst()}), \{darg.getFirst()})",
+                                                        STR."movq\t\{offset}(\{uarg.getFirst()}), \{darg.getFirst()}",
                                                 uses_,
                                                 defs_));
                                         newUses.add(new X64.VirtualReg.Reg(reg, ty));
@@ -78,7 +113,6 @@ public class StackAllocator {
                                     case X64.VirtualReg.Vid(Id id, X64.Type.T ty) -> {
                                         Id reg = regMap.get(id);
                                         int offset = tempMap.getOffset(id);
-                                        Id rbpReg = X64.Register.allRegs.get(6);
                                         List<X64.VirtualReg.T> uses_ = List.of(new X64.VirtualReg.Reg(reg, ty));
                                         List<X64.VirtualReg.T> defs_ = List.of(new X64.VirtualReg.Reg(rbpReg, ty));
                                         newInstrs.add(new X64.Instr.Singleton(X64.Instr.Kind.Store,
@@ -94,6 +128,37 @@ public class StackAllocator {
                         }
                     }
                 }
+
+                for (X64.Transfer.T t : transfer) {
+                    switch (t) {
+                        case X64.Transfer.Ret() -> {
+                            // Epilogue
+                            // Move rsp to rbp
+                            newInstrs.add(new X64.Instr.Singleton(X64.Instr.Kind.Move,
+                                    (uarg, darg) -> STR."movq\t%rbp, %rsp",
+                                    List.of(new X64.VirtualReg.Reg(rbpReg, new X64.Type.Int())),
+                                    List.of(new X64.VirtualReg.Reg(rspReg, new X64.Type.Int()))));
+
+                            // Restore callee-saved registers
+                            for (int i = X64.Register.calleeSavedRegs.size() - 1; i >= 0; i--) {
+                                Id reg = X64.Register.calleeSavedRegs.get(i);
+                                List<X64.VirtualReg.T> uses = List.of();
+                                List<X64.VirtualReg.T> defs = List.of(new X64.VirtualReg.Reg(reg, new X64.Type.Int()));
+                                newInstrs.add(new X64.Instr.Singleton(X64.Instr.Kind.Load,
+                                        (uarg, darg) -> STR."popq\t\{darg.getFirst()}",
+                                        uses,
+                                        defs));
+                            }
+                            // Restore base pointer register
+                            newInstrs.add(new X64.Instr.Singleton(X64.Instr.Kind.Load,
+                                    (uarg, darg) -> STR."popq\t%rbp",
+                                    List.of(),
+                                    List.of(new X64.VirtualReg.Reg(rbpReg, new X64.Type.Int()))));
+                        }
+                        default -> {}
+                    }
+                }
+
                 return new X64.Block.Singleton(label, newInstrs, transfer);
             }
         }
@@ -104,6 +169,7 @@ public class StackAllocator {
         int offset = 0;
         switch (function) {
             case X64.Function.Singleton(X64.Type.T retType, Id classId, Id methodId, List<X64.Dec.T> formals, List<X64.Dec.T> locals, List<X64.Block.T> blocks) -> {
+                Frame frame = new Frame(STR."\{classId.toString()} + . + \{methodId.toString()}");
                 for (X64.Dec.T formal : formals) {
                     switch (formal) {
                         case X64.Dec.Singleton(X64.Type.T ty, Id id) -> {
@@ -117,14 +183,19 @@ public class StackAllocator {
                         case X64.Dec.Singleton(X64.Type.T ty, Id id) -> {
                             tempMap.put(id, new TempMap.Position.InStack(offset));
                             offset -= X64.WordSize.bytesOfWord;
+                            frame.alloc();
                         }
                     }
                 }
 
 
                 List<X64.Block.T> newBlocks = new ArrayList<>();
+                boolean firstBlock = true;
                 for (X64.Block.T block : blocks) {
-                    newBlocks.add(allocBlock(block, tempMap));
+                    newBlocks.add(allocBlock(block, tempMap, firstBlock, frame));
+                    if (firstBlock) {
+                        firstBlock = false;
+                    }
                 }
                 return new X64.Function.Singleton(retType, classId, methodId, formals, locals, newBlocks);
             }
